b961e59ce8b84b5641069821cea56e12eda36702
Sairam R <saitechnology123123@gmail.com>
2022-03-13 17:07:55 +0530

    searchbox implementation

---
+++ src/components/SearchBox/SearchBox.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import './SearchBox.css'
+
+function SearchBox()
+{
+    return (
+        <div className="search-container">
+            <input placeholder="Type keywords" className="search-input" />
+        </div>
+
+    )
+
+}
+
+export default SearchBox;
